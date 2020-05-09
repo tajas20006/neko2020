dd52e3874747e51de07000eb08599e9cabf31d69
Yoshioka Shun <tajas2006@gmail.com>
2020-05-10 01:05:23 +0900

    Add user config support

--- neko2020/utils/configs.py
+++ neko2020/utils/configs.py
@@ -1,7 +1,6 @@
 import os
 from copy import deepcopy
 from functools import reduce
-
 import yaml
 
 from neko2020.utils import files
@@ -48,12 +47,14 @@ def get_string(config_path):
 default_config_file = os.path.join(
     files.get_project_root(), "config", "default_config.yml"
 )
+xdg_config_home = os.getenv(
+    "XDG_CONFIG_HOME", os.path.join(os.path.expanduser("~"), ".config")
+)
+user_config_file = os.path.join(xdg_config_home, "neko2020", "config.yml")
 
 with open(default_config_file) as f:
     config = yaml.load(f, Loader=yaml.BaseLoader)
-# if config_file is None:
-#     return default_config
-
-# with open(config_file) as f:
-#     user_config = yaml.load(f, Loader=yaml.BaseLoader)
-# return deep_merge(default_config, user_config)
+if os.path.exists(user_config_file):
+    with open(user_config_file) as f:
+        user_config = yaml.load(f, Loader=yaml.BaseLoader)
+    config = deep_merge(config, user_config)
